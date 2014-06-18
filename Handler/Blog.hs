407ce5a5124acca48e6de9c76d8db6a7568df41d
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2014-06-18 13:00:49 +0900

    Finish tutorial

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -1,9 +1,36 @@
-module Handler.Blog where
+module Handler.Blog
+  ( getBlogR
+  , postBlogR
+  )
+where
 
 import Import
 
+-- to use Html into forms
+
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+instance YesodNic App
+
+-- The view showing the list of articles
 getBlogR :: Handler Html
-getBlogR = error "Not yet implemented: getBlogR"
+getBlogR = do
+  -- Get the list of articles inside the database.
+  articles <- runDB $ selectList [] [Desc ArticleTitle]
+  -- We'll need the tow "objects": articleWidget aned enctype
+  -- to construct the form (see templates/articles.hamlet)
+  (articleWidget, enctype) <- generateFormPost entryForm
+  defaultLayout $ do
+    $(widgetFile "articles")
+
+entryForm :: Form Article
+entryForm = renderDivs $ Article
+  <$> areq textField "Title" Nothing
+  <*> areq nicHtmlField "Content" Nothing
 
 postBlogR :: Handler Html
-postBlogR = error "Not yet implemented: postBlogR"
+postBlogR = do
+  ((res, articleWidget), enctype) <- runFormPost entryForm
+  case res of
+    _ -> defaultLayout $ do
+      setTitle "Please correct your entry form"
+      $(widgetFile "articleAddError")
